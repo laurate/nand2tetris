27db4850cd06a4e6c3983e034e616a4ce8d48f6b
Laura Tessin <lauratessin@gmail.com>
2021-11-05 18:22:47 -0700

    feat: compare & record missing out files

--- compare.py
+++ compare.py
@@ -5,25 +5,37 @@ import os
 #######################################################################################################
 
 def compare_files(directory):
+    '''
+    Compare '.cmp' and '.out' files in directory and record missing '.out' files
+    '''
+
+    missing_files = []
 
     for file in os.listdir(directory):
 
-        if file.endswith('.out'):
-            out_file = file
-            cmp_file = os.path.basename(file).replace('.out', '.cmp')
+        if file.endswith('.cmp'):
+            cmp_file = file
+            out_file = os.path.basename(file).replace('.cmp', '.out')
 
-            print(f'Comparing {cmp_file} and {out_file} at {directory}...')
+            if os.path.exists(os.path.join(directory, out_file)):
 
-            with open(os.path.join(directory, out_file)) as f_1:
-                out_file_content = f_1.readlines()
+                print(f'Comparing {cmp_file} and {out_file} at {directory}...')
 
-            with open(os.path.join(directory, cmp_file)) as f_2:
-                cmp_file_content = f_2.readlines()
+                with open(os.path.join(directory, out_file)) as f_1:
+                    out_file_content = f_1.readlines()
 
-            if out_file_content == cmp_file_content:
-                print('✅ Files are equal')
+                with open(os.path.join(directory, cmp_file)) as f_2:
+                    cmp_file_content = f_2.readlines()
+
+                if out_file_content == cmp_file_content:
+                    print('✅ Files are equal')
+                else:
+                    print('⛔️ Files are not equal')
+            
             else:
-                print('⛔️ Files are not equal')
+                missing_files.append(out_file)
+
+    print('>>> Currently missing:', ', '.join(missing_files))
 
 if __name__ == '__main__':
 
