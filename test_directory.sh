938ec3ac9da7be159a2e90ebf02e9e8c1827eb32
Laura Tessin <lauratessin@gmail.com>
2021-11-10 15:55:48 -0800

    feat: adjust directory test script to run on macos

--- test_directory.sh
+++ test_directory.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-# test script from Miccah Castorina
+#!/bin/zsh
+
+# Test script by Miccah Castorina
+# Call with path to test directory, e.g. `$ ./test_directory.sh ./projects/01`
 
 # path to HardwareSimulator.sh
 hw_sim='/Users/lauratessin/recurse_center/nand2tetris/tools/HardwareSimulator.sh'
@@ -11,11 +13,9 @@ _rst='\e[39;49m'
 function compare_results {
     local have_file="$1.out"
     local want_file="$1.cmp"
-    local failure_line=$(wc --lines "$have_file" | cut -d' ' -f1)
-    echo -n '          '
-    head -n 1 "$want_file"
-    echo -e "expected:${_grn}" "$(sed -n ${failure_line}p "$want_file")$_rst"
-    echo -e "     got:${_red}" "$(tail -n 1 "$have_file")$_rst\n"
+    local failure_line=$(wc -l "$have_file" | cut -d' ' -f1)
+    echo -e ">>> expected\n${_grn}$(sed -n ${failure_line}p "$want_file")$_rst"
+    echo -e ">>> got\n${_red}$(tail -n 1 "$have_file")$_rst\n"
 }
 
 if [[ -z $1 ]]; then
@@ -30,13 +30,14 @@ fail_names=""
 for file in *.tst; do
     printf "%-20s" "${file%.tst}"
     result=$($hw_sim $PWD/$file 2>&1)
+    # echo $result
     if [[ $? -eq 0 ]]; then
         echo -e "${_grn}ok${_rst}"
         pass_count=$((pass_count + 1))
     elif [[ ! "$result" =~ "Comparison failure" ]]; then
         # syntax error
         echo -ne "${_red}fail: "
-        echo -e "$result${_rst}" | tail -n +2 | sed -e 's#/.*/projects/##'
+        echo -e "$result${_rst}"
         fail_names="$fail_names, ${file%.tst}"
     else
         echo -e "${_red}fail${_rst}"
