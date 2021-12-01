1df9d5717589ea5f8fb04366b6f8a4013f29d9aa
Laura Tessin <lauratessin@gmail.com>
2021-11-30 18:57:26 -0800

    fix: clean up

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,14 +6,10 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
-
-// loop over pixels -> if key pressed -> value is not 0 -> write all pixels black (1); otherwise white (0)
 
 (BLACK)
     @KBD
