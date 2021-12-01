78c85f7fade3ee5e1a0f4c54d5b29f1f9b67a9f3
laurate <lauratessin@gmail.com>
2021-12-01 10:26:58 -0800

    Project/04 (#5)
    * task: multiplier
    
    * task in progress: fill
    
    * fix: clean up
    
    * fix: use 16bit value and fix off-by-one

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,68 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BLACK)
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ   // goto WHITE if no key pressed
+
+    @color
+    M=-1
+
+    @8191
+    D=A
+    @iterator   // reset iterator to 8191
+    M=D
+
+    @FILLSCREEN
+    0;JMP   // fill screen with black pixels
+
+(WHITE)
+    @KBD
+    D=M
+    @BLACK
+    D;JNE   // goto BLACK if key pressed
+
+    @color
+    M=0
+
+    @8191
+    D=A
+    @iterator   // reset iterator to 8191
+    M=D
+
+    @FILLSCREEN
+    0;JMP   // fill screen with white pixels
+
+(FILLSCREEN)
+    // iterate over all the pixels -> fill with color
+    @iterator
+    D=M
+    @SCREEN
+    D=A+D
+    @currentpixel
+    M=D
+
+    @color
+    D=M
+    @currentpixel
+    A=M
+    M=D
+
+    @iterator
+    M=M-1
+
+    // check if iterator has reached end of screen -> go back to check keyboard input
+    @iterator
+    D=M
+    @BLACK
+    D;JLT
+
+    @FILLSCREEN
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,25 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @R2
+    M=0 // set product to 0
+
+(LOOP)
+    @R1
+    D=M
+    @END
+    D;JEQ   // goto END if iterator = 0
+
+    @R0
+    D=M
+    @R2
+    M=M+D   // increase by multiplier
+
+    @R1
+    M=M-1   // reduce iterator
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP  // Infinite loop
